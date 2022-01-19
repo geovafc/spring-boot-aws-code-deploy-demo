c0c953717b65cb8960164c5326359b0367013088
Geovane Freitas <geovane.freitasc@gmail.com>
2022-01-19 07:06:54 -0300

    return api

--- src/main/java/com/sha/awscodedeploydemo/controller/ApiController.java
+++ src/main/java/com/sha/awscodedeploydemo/controller/ApiController.java
@@ -24,10 +24,4 @@ public class ApiController
         return ResponseEntity.ok("List of users {}");
     }
 
-    @GetMapping("api/usersById")
-    public ResponseEntity<?> usersById()
-    {
-        return ResponseEntity.ok("List of users By Id{}");
-    }
-
 }
